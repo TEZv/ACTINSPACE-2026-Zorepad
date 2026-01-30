02ae364a3ff61c83f5f7f59479fb2c51b51ab19e
Fanuk <illyamak24@gmail.com>
2026-01-30 16:03:38 +0200

    update docker, connected db in cs, fix syntax

--- Zorepad-ACTINSPACE-2026-project/Program.cs
+++ Zorepad-ACTINSPACE-2026-project/Program.cs
@@ -7,16 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+var connectionString = "Host=172.16.41.78;Port=5432;Database=cosmic_db;Username=admin;Password=<secret_password>";
+
+// register DbContext BEFORE calling Build()
+builder.Services.AddDbContext<AppDbContext>(options =>
+   options.UseNpgsql(connectionString, o => o.UseNetTopologySuite()));
+
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-var connectionString = "Host=localhost;Port=5432;Database=stellar_library;Username=admin;Password=space_password_123";
-
-//builder.Services.AddDbContext<AppDbContext>(options =>
-  //  options.UseNpgsql(connectionString, o => o.UseNetTopologySuite()));
 
 app.Run();
